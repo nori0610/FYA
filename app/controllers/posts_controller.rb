496a32781a3b612b30aa82f9b62d0da1ec591bfc
nori0610 <39909534+nori0610@users.noreply.github.com>
2018-07-29 22:27:05 +0900

    add_title

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,24 +1,25 @@
 class PostsController < ApplicationController
   before_action :authenticate_user
   before_action :ensure_correct_user, {only: [:edit, :update, :destroy]}
-  
+
   def index
     @posts = Post.all.order(created_at: :desc)
   end
-  
+
   def show
     @post = Post.find_by(id:params[:id])
     @user = @post.user
     @likes_count = Like.where(post_id: @post.id).count
   end
-  
+
   def new
     @post = Post.new
   end
-  
+
   def create
+    @post = Post.new(title: params[:title], user_id: @current_user.id)
     @post = Post.new(content: params[:content], user_id: @current_user.id)
-    
+
     if @post.save
       flash[:notice] = "投稿を作成しました"
       redirect_to("/posts/index")
@@ -26,15 +27,15 @@ class PostsController < ApplicationController
       render("posts/new")
     end
   end
-  
+
   def edit
     @post = Post.find_by(id: params[:id])
   end
-  
+
   def update
     @post = Post.find_by(id: params[:id])
     @post.content = params[:content]
-    
+
     if @post.save
       flash[:notice] = "投稿を編集しました"
       redirect_to("/posts/index")
@@ -42,14 +43,14 @@ class PostsController < ApplicationController
       render("posts/edit")
     end
   end
-  
+
   def destroy
     @post = Post.find_by(id: params[:id])
     @post.destroy
     flash[:notice] = "投稿を削除しました"
     redirect_to("/posts/index")
   end
-  
+
   def ensure_correct_user
     @post = Post.find_by(id: params[:id])
     if @post.user_id != @current_user.id
@@ -57,5 +58,5 @@ class PostsController < ApplicationController
       redirect_to("/posts/index")
     end
   end
-  
+
 end
